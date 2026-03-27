5e67b5a5b76faddf1f9c577ad5cb835a8184b3a0
DavierAL <davierlopez18dl@gmail.com>
2026-03-27 05:32:24 -0500

    feat: add order schema validation using zod and update package dependencies

---
+++ public/js/shared/orderSchema.js
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const valueEmpty = (name, type) => {
+    return `${type === "m" ? "El" : "La"} ${name} es requerida`
+}
+
+export const orderSchema = z.object({
+    email: z.email("Email inválido"),
+    name: z.string().min(3, { error: valueEmpty("nombre", "m") }),
+    lastName: z.string().min(3, { error: valueEmpty("apellido", "m") }),
+    phone: z.string().min(9, { error: valueEmpty("teléfono", "m") }),
+    company: z.string().optional(),
+    address: z.string().min(3, { error: valueEmpty("dirección", "f") }),
+    city: z.string().min(1, { error: valueEmpty("ciudad", "f") }),
+    country: z.string().min(1, { error: valueEmpty("país", "m") }),
+    region: z.string().min(1, { error: valueEmpty("región", "f") }),
+    postalCode: z.string().min(1, { error: valueEmpty("código postal", "m") }),
+    paymentMethod: z.string().min(1, { error: valueEmpty("método de pago", "m") }),
+})
